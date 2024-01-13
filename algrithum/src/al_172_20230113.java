b07f3e9337c562232c7bae216d106c5194856ef2
xiaolin0701 <2725344170@qq.com>
2024-01-13 23:03:01 +0800

    2024/1/13

---
+++ algrithum/src/al_172_20230113.java
@@ -0,0 +1,17 @@
+/**
+ *@Author ylwang
+ *@Date   2024/1/13
+ */
+package src;
+
+public class al_172_20230113 {
+    //给你一个非负整数 x ，计算并返回 x 的 算术平方根 。
+    public int mySqrt(int x) {
+        for(long i=0;i<=x;i++){
+            if(i*i<=(long)x&&(i+1)*(i+1)>(long)x)
+                return (int)i;
+        }
+
+        return 0;
+    }
+}
